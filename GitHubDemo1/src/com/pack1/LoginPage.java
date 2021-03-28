983e82a194ae7ff12bd04b6ff36dfa3c66b7b65d
dsandie <dsandie@gmail.com>
2021-03-28 19:06:59 +0530

    First Commit

---
+++ GitHubDemo1/src/com/pack1/LoginPage.java
@@ -0,0 +1,12 @@
+package com.pack1;
+
+public class LoginPage {
+	
+	public void loginpage() {
+
+	System.out.println("This is login page");
+	System.out.println("This is first commit");
+	
+	
+	}
+}
